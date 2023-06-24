c7337fd275e51f79a8ae833d4f1e18432b12cb07
mhrrsn <miaharrison24@gmail.com>
2023-06-24 11:34:01 +0800

    Add rollDice function

--- dice_script.js
+++ dice_script.js
@@ -1,1 +1,12 @@
-alert("Hello!")
+// add a click handler on button to call function
+
+//crate function when button clicked to roll dice
+
+function rollDice() {
+//step 1: get number from input
+    let number = document.getElementById("dice-input").value;
+//step 2: generate random whole number from 1 to n
+    let result = Math.ceil(Math.random() * number);
+//step 3: display results on screen
+    document.getElementById("result").innerHTML = result
+}
